8a6a8e463ec52adb50dee25d8496ec8a5db3d415
mudit gupta <es14btech11012@iith.ac.in>
2016-05-20 14:57:35 +0530

    completed pca mini project

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -72,15 +72,13 @@ print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.
 t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
-
 eigenfaces = pca.components_.reshape((n_components, h, w))
-
+#print "explained variance ration: ",pca.explained_variance_ratio_
 print "Projecting the input data on the eigenfaces orthonormal basis"
 t0 = time()
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
-
 
 ###############################################################################
 # Train a SVM classification model
@@ -91,8 +89,8 @@ param_grid = {
          'C': [1e3, 5e3, 1e4, 5e4, 1e5],
           'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
           }
-# for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+# for sklearn version 0.16 or prior, the class_weight parameter value is 'auto' and for 0.17 and prior "balanced"
+clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
 clf = clf.fit(X_train_pca, y_train)
 print "done in %0.3fs" % (time() - t0)
 print "Best estimator found by grid search:"
